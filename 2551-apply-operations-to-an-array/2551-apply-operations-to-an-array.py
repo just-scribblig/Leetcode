dcafa1c0603016666bb57665fa89f36bfafe16c2
just-scribblig <hailemariamlibassie55@gmail.com>
2025-04-06 18:09:26 +0300

    Time: 4 ms (14.24%), Space: 18.1 MB (7.74%) - LeetHub

---
+++ 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.py
@@ -0,0 +1,10 @@
+class Solution:
+    def applyOperations(self, nums: List[int]) -> List[int]:
+        for i in range(len(nums)-1):
+            if nums[i] == nums[i+1]:
+                nums[i] = 2*nums[i]
+                nums[i+1] = 0
+        zeros = nums.count(0)
+        nums = [i for i in nums if i != 0]
+        nums.extend([0]*zeros)
+        return nums
